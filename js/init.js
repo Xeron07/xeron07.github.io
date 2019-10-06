886de93d6031bf62d7a798561800e6af8870d08b
Nihal <ni.xeron07@gmail.com>
2019-10-06 21:48:55 +0600

    init change

--- js/init.js
+++ js/init.js
@@ -4,6 +4,28 @@
 /*
 -----------------------------------------------------------------------------------*/
 
+/*----------------------------------------------------*/
+/*==========reCAPTCHA ===================*/
+/*------------------------------------------------------*/
+var onloadCallback = function() {
+  // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
+  // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
+  var widgetId1;
+  var widgetId2;
+
+  widgetId1 = grecaptcha.render("example1", {
+    sitekey: "6LfCJrwUAAAAAF3wjZPAG5cbmpbc_UTIeRVVKtFO",
+    theme: "light"
+  });
+  widgetId2 = grecaptcha.render(document.getElementById("example2"), {
+    sitekey: "6LfCJrwUAAAAAF3wjZPAG5cbmpbc_UTIeRVVKtFO"
+  });
+  grecaptcha.render("example3", {
+    sitekey: "6LfCJrwUAAAAAF3wjZPAG5cbmpbc_UTIeRVVKtFO",
+    theme: "dark"
+  });
+};
+
 jQuery(document).ready(function($) {
   /*----------------------------------------------------*/
   /* FitText Settings
@@ -16,28 +38,6 @@ jQuery(document).ready(function($) {
       maxFontSize: "90px"
     });
   }, 100);
-
-  /*----------------------------------------------------*/
-  /*==========reCAPTCHA ===================*/
-  /*------------------------------------------------------*/
-  var onloadCallback = function() {
-    // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
-    // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
-    var widgetId1;
-    var widgetId2;
-
-    widgetId1 = grecaptcha.render("example1", {
-      sitekey: "6LfCJrwUAAAAAF3wjZPAG5cbmpbc_UTIeRVVKtFO",
-      theme: "light"
-    });
-    widgetId2 = grecaptcha.render(document.getElementById("example2"), {
-      sitekey: "6LfCJrwUAAAAAF3wjZPAG5cbmpbc_UTIeRVVKtFO"
-    });
-    grecaptcha.render("example3", {
-      sitekey: "6LfCJrwUAAAAAF3wjZPAG5cbmpbc_UTIeRVVKtFO",
-      theme: "dark"
-    });
-  };
 
   /*----------------------------------------------------*/
   /* Smooth Scrolling
